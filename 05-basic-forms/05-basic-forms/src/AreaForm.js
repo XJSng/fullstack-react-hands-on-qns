cf876e1729e084cd1d75fb511198ea83ae342278
Xu Jie Sng <sngxujie@gmail.com>
2024-03-09 08:04:57 +0000

    completed practice 5

--- 05-basic-forms/05-basic-forms/src/AreaForm.js
+++ 05-basic-forms/05-basic-forms/src/AreaForm.js
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function AreaForm(){
-  
-    return (
-      <React.Fragment>
-        <div>
-          <label>Width:</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Height:</label>
-          <input type="text" />
-        </div>
-        <div>Area = </div>
-      </React.Fragment>
-    )
-  
-    const updateWidth =  (event) => {
-        
-    }
+export default function AreaForm() {
+  const [width, setWidth] = useState("")
+  const [height, setHeight] = useState("")
 
-    const updateHeight = (event) => {
-         
-    }
+  // Calculate Area here
+  let calculatedArea = 0
+  if (!width && !height) {
+    calculatedArea = "Please enter the width & height.";
+  }
+  else if (!width) {
+    calculatedArea = "Please enter the width.";
+  } else if (!height) {
+    calculatedArea = "Please enter the height.";
+  }
+  else {
+    calculatedArea = parseInt(width) * parseInt(height)
+  }
+
+
+  return (
+    <React.Fragment>
+      <div>
+        <label>Width:</label>
+        <input type="text"
+          value={width}
+          onChange={(e) => {
+            setWidth(() => e.target.value)
+          }} />
+      </div>
+      <div>
+        <label>Height:</label>
+        <input type="text"
+          value={height}
+          onChange={(e) => {
+            setHeight(() => e.target.value)
+          }} />
+      </div>
+      <div onChange={(width, height) => {
+        calculatedArea(width, height)
+      }}>Area = {calculatedArea}</div>
+    </React.Fragment>
+  )
+
+  const updateWidth = (event) => {
+
+  }
+
+  const updateHeight = (event) => {
+
+  }
 
 }
 
