3da419bfa789d391a9efcfe5c1ad60501c50df00
Xu Jie Sng <sngxujie@gmail.com>
2024-03-12 06:51:37 +0000

    completed practice 9

--- 09-mcq/09-mcq/src/Question.js
+++ 09-mcq/09-mcq/src/Question.js
@@ -8,17 +8,44 @@ export default function Question() {
         <section id="qns1">
         <h1>Question 1: Which island is Mount Faber, Singapore, located on?</h1>
         <ul>
-          <li>
-            <input name="qns1answer" type="radio" value="A"/><label>A. Palau Ubin</label>
+          <li style={{
+              listStyle: "none"
+            }}>
+            <input name="qns1answer"
+            type="radio"
+            value="A"
+            checked={answer === "A"}
+            onChange={() => {
+              setAnswer("A")
+          }}/><label>A. Palau Ubin</label>
           </li>
-          <li>
-            <input name="qns1answer" type="radio" value="B"/><label>B. Palau Ujong</label>
+          <li style={{
+              listStyle: "none"
+            }}>
+            <input name="qns1answer" type="radio" value="B" 
+            checked={answer === "B"}
+            onChange={() => {
+              setAnswer("B")
+          }}/><label>B. Palau Ujong</label>
           </li>
-          <li>
-            <input name="qns1answer" type="radio" value="C"/><label>C. Palau Tekong</label>
+          <li style={{
+              listStyle: "none"
+            }}>
+            <input name="qns1answer" type="radio" value="C"
+            checked={answer === "C"}
+            onChange={() => {
+              setAnswer("C")
+          }}/><label>C. Palau Tekong</label>
           </li>
-          <li>
-            <input name="qns1answer" type="radio" value="D"/><label>D. Saint John Island</label>
+          <li style={{
+              listStyle: "none"
+            }}>
+            <input name="qns1answer" type="radio" value="D"
+            
+            checked={answer === "D"}
+            onChange={() => {
+              setAnswer("D")
+          }}/><label>D. Saint John Island</label>
           </li>
         </ul>
       </section>
