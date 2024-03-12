ee9887bed7b5084a740845d9acf51826cbcee68a
Xu Jie Sng <sngxujie@gmail.com>
2024-03-12 03:03:57 +0000

    completed practice 8

--- 08-error-validation/08-error-validation/src/RegisterForm.js
+++ 08-error-validation/08-error-validation/src/RegisterForm.js
@@ -1,28 +1,63 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RegisterForm() {
 
-    const [userName, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [userName, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-      <React.Fragment>
-        <div>
-            <label>Username:</label>
-            <input type="text" name="username"/>
-            <span class="error"></span>
-        </div>
-        <div>
-            <label>Email:</label>
-            <input type="text" name="email"/>
-            <span class="error"></span>
-        </div>
-        <div>
-            <label>Password:</label>
-            <input type="text" name="password"/>
-            <span class="error"></span>
-        </div>
-      </React.Fragment>
-    )
+  const usernameValidation = () => {
+    if (userName.length < 4 || userName.length > 10) {
+      return "Username must be 4 to 10 characters long."
+    }
+  }
+
+  let passwordConditions = ["!", "@", "#", "$", "%", "^", "&", "*"]
+  const hasDigit = /[0-9]/.test(password);
+  const hasAlphabet = /[a-zA-Z]/.test(password);
+
+  const passwordValidation = () => {
+    if (!passwordConditions.some(e => password.includes(e))) {
+      return "Password must have at least one special character"
+    } else if (!hasDigit) {
+      return "Password must have at least one digit"
+    } else if (!hasAlphabet) {
+      return "Password must have at least one alphabet"
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <div className='container'>
+        <label className='mt-3'>Username:</label>
+        <input type="text"
+          name="username"
+          value={userName}
+          onChange={(e) => { setUsername(e.target.value) }} />
+        <span class="error">{usernameValidation()}</span>
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="text" name="email"
+        className='mt-3'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }} />
+        <span class="error">{
+          (!email.includes("@") ? "Email Address must contain an '@' character" : "")}</span>
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" name="password"
+        className='mt-3'
+        value={password}
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }} />
+        <span className="ms-3" class="error">{passwordValidation()}</span>
+      </div>
+    </React.Fragment>
+  )
 }
