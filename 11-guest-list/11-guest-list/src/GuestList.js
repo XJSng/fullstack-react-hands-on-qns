590b632c4b416ca702e32a492c2a60f65addf9ec
Xu Jie Sng <sngxujie@gmail.com>
2024-03-13 04:04:35 +0000

    completed practice 11

--- 11-guest-list/11-guest-list/src/GuestList.js
+++ 11-guest-list/11-guest-list/src/GuestList.js
@@ -13,7 +13,12 @@ export default function GuestList() {
 
     return (
         <React.Fragment>
-
+            <h1>Guest List:</h1>
+<ul>
+{guests.map((e)=>{
+    return <li>{e}</li>
+})
+}</ul>
         </React.Fragment>
     )
 }
