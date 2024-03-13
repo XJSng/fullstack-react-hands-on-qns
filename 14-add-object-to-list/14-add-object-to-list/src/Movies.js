dbc6a581566ce65ee0ff7e4534d6a5e675d66c4f
Xu Jie Sng <sngxujie@gmail.com>
2024-03-13 11:10:34 +0000

    completed practice 14

--- 14-add-object-to-list/14-add-object-to-list/src/Movies.js
+++ 14-add-object-to-list/14-add-object-to-list/src/Movies.js
@@ -1,35 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-export default function Movies(){
+export default function Movies() {
 
     const [movies, setMovies] = useState(
         [
             {
-            'title':'Star Wars IV: A New Hope',
-            'director': 'George Lucas'
+                'title': 'Star Wars IV: A New Hope',
+                'director': 'George Lucas'
             },
             {
-            'title':'The Lord of the Rings: The Fellowship of the Ring',
-            'director':'Peter Jackson'
+                'title': 'The Lord of the Rings: The Fellowship of the Ring',
+                'director': 'Peter Jackson'
             }
         ]
     )
 
     const [title, setTitle] = useState("");
     const [director, setDirector] = useState("");
-  
+
 
     function addMovie() {
+        const cloned = [...movies, { title, director }]
+        setMovies(cloned)
+    }
 
-
+    function removeMovie(e) {
+        const indexToRemove = movies.findIndex(m => m == e.target.value);
+        const cloned = [...movies.slice(0, indexToRemove),
+        (movies.slice(indexToRemove + 1))]
+        setMovies(cloned)
     }
 
     return <React.Fragment>
-        {movies.map( m => (
+        {movies.map(m => (
             <React.Fragment>
                 <div class="movie">
                     <h1>{m.title}</h1>
                     <h2>Directed by {m.director}</h2>
+                    <button onClick={removeMovie}>Delete</button>
                 </div>
             </React.Fragment>
         ))}
@@ -37,11 +45,11 @@ export default function Movies(){
         <h3>Add New Movie</h3>
         <div>
             <label>Movie Title:</label>
-            <input type="text" name="title" value={title} onChange={(event) => setTitle(event.target.value)}/>
+            <input type="text" name="title" value={title} onChange={(event) => setTitle(event.target.value)} />
         </div>
         <div>
             <label>Director</label>
-            <input type="text" name="director" value={director} onChange={(event) => setDirector(event.target.value)}/>
+            <input type="text" name="director" value={director} onChange={(event) => setDirector(event.target.value)} />
         </div>
         <button onClick={addMovie}>Add Movie</button>
     </React.Fragment>
