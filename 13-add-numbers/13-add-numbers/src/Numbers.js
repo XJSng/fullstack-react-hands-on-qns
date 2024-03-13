9efd1f198d722c91998531f7964287250aac9116
Xu Jie Sng <sngxujie@gmail.com>
2024-03-13 08:45:18 +0000

    completed practice 13

--- 13-add-numbers/13-add-numbers/src/Numbers.js
+++ 13-add-numbers/13-add-numbers/src/Numbers.js
@@ -3,33 +3,52 @@ import React, {useState} from 'react'
 export default function Numbers () {
   
     const [allNumbers, setAllNumbers] = useState([1,3,5,7]);
+    const [userNumber, setUserNumber] = useState()
   
 
     const addRandom = ()=> {
         // 1. clone the allNumbers array in the state
-
+       
         // 2. add a random number to the back of the clone
+           // 3. set the cloned array back into the state
         let randomNumber = Math.floor(Math.random() * 10 + 1)
-
-        // 3. set the cloned array back into the state
+        const cloned = [...allNumbers, randomNumber]     
+        setAllNumbers(cloned)
     }
 
     const addNumber = () => {
-        
+        const hasDigit = /[0-9]/.test(userNumber)
+        const hasAlphabet = /[a-zA-Z]/.test(userNumber);
+
+
+        if (!hasDigit || hasAlphabet) {
+            return alert("Please enter numbers only!")
+        }
+
+        else {
+        const cloned = [...allNumbers, userNumber]
+        setAllNumbers(cloned)
+        }
     }
 
 
     return <React.Fragment>
         <ul>
-            {allNumbers? allNumbers.map(number=><li>{number}</li>) 
+            {allNumbers? allNumbers.map(number=><li
+            style={{
+                listStyle: "none"
+            }}>{number}</li>) 
                         : <p>Loading...</p>}
         </ul>
         <div>
             <button onClick={addRandom}>Add Random</button>
         </div>
         <div>
-            <input type="text" name="newNumber"/>
-            <button>Add</button>
+            <input type="text" name={userNumber}
+            onChange={(e)=>{
+                setUserNumber(()=> e.target.value)
+            }}/>
+            <button onClick={addNumber}>Add</button>
         </div>
     </React.Fragment>
 
